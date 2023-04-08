9b0bbc9a3779313d25e840ebb38faf288f192a42
agil1998 <121118552+agil1998@users.noreply.github.com>
2023-04-08 20:54:41 +0700

    Add files via upload
    FolderApp

---
+++ app/viewsApp/web/profil.php
@@ -0,0 +1,41 @@
+<!-- bradcam_area_start -->
+    <div class="bradcam_area breadcam_bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="bradcam_text text-center">
+                        <h3>Profil</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bradcam_area_end -->
+
+    <!-- pet_care_area_start  -->
+    <div class="pet_care_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-6">
+                    <div class="pet_thumb">
+                        <img src="<?= BASEURL; ?>/assets_web/img/about/zakat2.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-md-6">
+                    <div class="pet_info">
+                        <div class="section_title">
+                            <h3><span>Apa itu </span> <br>
+                                BAZ Masjid Raya Kebayoran Residence?</h3>
+                            <p><B>Badan Zakat Masjid Raya Kebayoran Residence
+                            </B> adalah salah satu aplikasi yang memiliki fungsi untuk membantu kaum muslimin untuk menunaikan zakatnya.</p>
+                            <a href="<?=BASEURL;?>/Web/donasi" class="boxed-btn3">Yuk Donasi !</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- pet_care_area_end  -->
+    
+
+    
